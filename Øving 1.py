6cc5d95e2984218921aecb66f4b7922b80845960
Hatlelol <sondre.hatlehol@hotmail.com>
2018-09-02 18:23:02 +0200

    Add files via upload

---
+++ Øving 1.py
@@ -0,0 +1,23 @@
+print("Jeg elsker ITGK")
+
+print("Norge")
+print()
+print("Areal (kv.km):", 385180) #Her setter jeg komma mellom tallene og teksten for å skille dem
+print("Folketall (mill.):", 5.3)
+
+print('"Jeg elsker itgk" ropte studentene da 1c fungerte') #Bruker '' rundt teksten for å kunne bruke ""
+
+#print('hei")
+print("""Noen barn sto og hang ved lekeplassen.""")
+print("""Diskusjonstemaet deres var noe uventet.""")
+print("""- Hvorfor heter'e "Python"?""")
+print("""- Var'e slanger som laget det? - Nei, Guido van Rossum.""")
+print("""- Likte slanger kanskje da? - Nei, digga "Monty Python".""")
+print("""- Hva er det? Et fjell?""")
+print("""- Nei, engelsk komigruppe. Begynte i '69.""")
+print("""- Wow! Var'e fremdeles dinosaurer da?""")
+
+print("Heihei, jeg vil visst ikke kompilere jeg :(")
+print("Halla, så 'bra' du ser ut i dag")
+print("Hei på deg")
+print("Er ikke dette gøy?")
